739aa8c9953827b99a1cd8eb2a261b1a26145c48
sola0 <sola0419@likelion.org>
2021-08-03 09:40:12 +0900

    21/08/03 Tue commit 2

--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -233,4 +233,15 @@ let magicGrid = new MagicGrid({
   static: true,
   useMin: true
 });
+
+
+var masonrys = document.getElementsByTagName("img")
+
+for(let i=0; masonrys.length; i++){
+    masonrys[i].addEventListener('load', function () {
+      magicGrid.positionItems();
+    }, false)
+}
+
+
 magicGrid.listen();
