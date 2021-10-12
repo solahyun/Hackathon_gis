83df173dda1157c1fad3a330d2faeeaa7c8d9b1b
sola0 <sola0419@likelion.org>
2021-10-13 03:44:06 +0900

    21/10/12 commit 5

--- trendapp/urls.py
+++ trendapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from trendapp.views import BasicTemplateView, TrendListView
+from trendapp.views import BasicTemplateView, TrendListView, let_write
 
 app_name = 'trendapp'
 
 urlpatterns = [
     path('info/', BasicTemplateView.as_view(), name='info'),
     path('list/', TrendListView.as_view(), name='list'),
+    path('let_write/', let_write, name='let_write'),
 ]
--- trendapp/views.py
+++ trendapp/views.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 
@@ -12,11 +13,11 @@ class TrendListView(ListView):
     template_name = 'trendapp/bukgu.html'
     model = NewModel
 
-
 def let_write(request):
     if request.method == "POST":
-        if request.POST.get(""):
+        if request.POST.get("input_text"):
+            image = "{% static 'img/1.png' %}"
 
-            return render(request, 'trendapp/bukgu.html')
+            return render(request, context={'text': image})
     else:
-        return render(request, 'trendapp/bukgu.html')
+        return render(request, context={'text': ""})
